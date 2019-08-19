53a3d7fb8ba5de64347351a43efc78aa126e262b
Arnaud de Castelbajac <adecastelbajac@gmx.com>
2019-08-19 15:45:35 +0200

    routes artworks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'artworks#index'
+  resources :artworks
 end
