7339a289adfb79659d17774c2010cec3bacc192b
Arnaud de Castelbajac <adecastelbajac@gmx.com>
2019-08-19 16:00:53 +0200

    new and create actions

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,2 +1,15 @@
 class ArtworksController < ApplicationController
+  def new
+    @artwork = Artwork.new
+  end
+
+  def create
+    @artwork = Artwork.new(artwork_params)
+  end
+
+  private
+
+  def artwork_params
+    params.require(:artwork)
+  end
 end
