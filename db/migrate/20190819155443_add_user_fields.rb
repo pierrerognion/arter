749b3f77cb0fb8a617a566806fddbdf5238146b6
Pierre Rognion <pierrerognion@gmail.com>
2019-08-19 17:56:34 +0200

    added user fields

---
+++ db/migrate/20190819155443_add_user_fields.rb
@@ -0,0 +1,8 @@
+class AddUserFields < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :birth_date, :date
+    add_column :users, :avatar, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_155117) do
+ActiveRecord::Schema.define(version: 2019_08_19_155443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,10 @@ ActiveRecord::Schema.define(version: 2019_08_19_155117) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birth_date"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
