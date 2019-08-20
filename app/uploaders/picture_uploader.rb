aa221768ca93e6ba21780915c35bae63567706a7
Arnaud de Castelbajac <adecastelbajac@gmx.com>
2019-08-20 15:44:46 +0200

    vue picture pour les cards index

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,3 +1,10 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+  process eager: true
+
+  process convert: 'jpg'
+
+  version :index_card do
+    cloudinary_transformation width: 400, height: 400, crop: :crop
+  end
 end
