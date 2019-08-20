ac82cfff6096cd26615d740e2a80c340075ba298
Briyan Cessac <briyan.cessac@gmail.com>
2019-08-20 15:54:58 +0200

    actualisation of the format of the new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_081258) do
+ActiveRecord::Schema.define(version: 2019_08_20_133732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_081258) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "artist"
+    t.string "country"
     t.index ["user_id"], name: "index_artworks_on_user_id"
   end
 
