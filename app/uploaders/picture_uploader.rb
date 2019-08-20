43ca9b5efcb2605b3ad7fae320f688473cbe2417
Arnaud de Castelbajac <adecastelbajac@gmx.com>
2019-08-20 16:04:40 +0200

    picture formatage pour la show

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,4 +7,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   version :index_card do
     cloudinary_transformation width: 400, height: 400, crop: :crop
   end
+
+  version :show_artwork do
+    cloudinary_transformation height: 700, width: 700, background: "auto:predominant", crop: :pad
+  end
 end
