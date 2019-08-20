a529cd7a141cf24c8fe3e4687feac8dc6c470ad9
Arnaud de Castelbajac <adecastelbajac@gmx.com>
2019-08-20 10:16:04 +0200

    add artist column to artwork table

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,6 +2,7 @@ class Artwork < ApplicationRecord
   has_many :bookings
   belongs_to :user
   validates :name, presence: true, uniqueness: true
+  validates :artist, presence: true
   validates :price, presence: true
   validates :description, presence: true
   validates :picture, presence: true
