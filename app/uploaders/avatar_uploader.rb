00c839e7020d788eced39bfb16c86b109fea0aac
clement huet <clemhu@hotmail.com>
2019-08-22 16:29:21 +0200

    avatar upload

---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,14 @@
+class AvatarUploader < CarrierWave::Uploader::Base
+  include Cloudinary::CarrierWave
+  # process eager: true
+
+  # process convert: 'jpg'
+
+  # version :index_card do
+  #   cloudinary_transformation width: 400, height: 400, crop: :thumb
+  # end
+
+  # version :show_artwork do
+  #   cloudinary_transformation height: 700, width: 700, background: "auto:predominant", crop: :pad
+  # end
+end
